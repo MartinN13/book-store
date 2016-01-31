948fdebae02dd0709c210bf216bb672107260813
MartinN13 <martinnabhan@me.com>
2016-01-31 02:11:51 +0100

    Add and style first banner entry

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "460x290>", :thumb => "230x145>" } 
+  has_attached_file :image, :styles => { :medium => "366x244>", :thumb => "230x145>" } 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
 end
